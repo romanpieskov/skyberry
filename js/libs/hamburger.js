437bd8028b25095c1ec1d14b185bdaaea27792dc
BerezhniyDmitro <berezhniy.d@gmail.com>
2016-04-05 14:36:14 +0300

    пофиксил баг с выпадением меню

--- js/libs/hamburger.js
+++ js/libs/hamburger.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     var $hamburger = $(".hamburger");
      $hamburger.on("click", function(e) {
        $hamburger.toggleClass("is-active");
-       $("#menu").toggleClass("mobilemenu");
-       $("#menu").toggleClass("menu");
+       $("#primary-menu").toggleClass("mobilemenu");
+       $("#primary-menu").toggleClass("menu");
    });
 });
