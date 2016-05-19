fd0b34e346467d61f245e0cf54439a977e2521ad
BerezhniyDmitro <berezhniy.d@gmail.com>
2016-05-19 13:19:32 +0300

    несколько правок

--- js/libs/hamburger.js
+++ js/libs/hamburger.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     var $hamburger = $(".hamburger");
      $hamburger.on("click", function(e) {
        $hamburger.toggleClass("is-active");
-       $("#primary-menu").toggleClass("mobilemenu");
-       $("#primary-menu").toggleClass("menu");
+       $(".primary-menu").toggleClass("mobilemenu");
+       $(".primary-menu").toggleClass("menu");
    });
 });
